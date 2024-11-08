80baa41abc2bf1cbdec82762fda6f315008307b1
VHAC code <vachaspati1997@gmail.com>
2024-11-08 07:17:12 +0530

    Add project files with .gitignore in place

---
+++ Frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <ChatProvider>
+          <App />
+        </ChatProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
